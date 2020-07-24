8b730052b6e874ea1a252086e2906a3b83bdcbd4
anawat <s5953410018@sau.ac.th>
2020-07-24 17:36:01 +0700

    avatarPosition

--- assets/scritp/3rdUI.ts
+++ assets/scritp/3rdUI.ts
@@ -38,16 +38,19 @@ export default class NewClass extends cc.Component {
     // onLoad () {}
 
     start() {
-        for(let i=0;i<=5;i++){
-            this.randomAvatar()
+        for (let i = 0; i <= 4; i++) {
+            this.randomAvatar(i)
+            console.log(i);
+
         }
 
     }
-    seq = [0,1,2,3,4,5]
+    seq = [0, 1, 2, 3, 4, 5]
 
-    avatarName = ['captain_america_front', 'chuwit_front', 'kim_front', 'mark_front', 'monlo_front', 'pablo_front', 'prayut_front', 'tanee_front', 'trump_front', 'wonder_woman_front', 'xi_jinping_front']
+    avatarName1 = ['captain_america_front', 'chuwit_front', 'kim_front', 'mark_front', 'monlo_front', 'pablo_front', 'prayut_front', 'tanee_front', 'trump_front', 'wonder_woman_front', 'xi_jinping_front']
+    avatarName2 = ['captain_america_side', 'chuwit_side', 'kim_side', 'mark_side', 'monlo_side', 'pablo_side', 'prayut_side', 'tanee_side', 'trump_side', 'wonder_woman_side', 'xi_jinping_side']
 
-    path = 'Avatar/Avatar_Front'
+    path = ['Avatar/Avatar_Front','Avatar/Avatar_Side']
 
     getImageRes(node, url, name) {
         cc.loader.loadRes(url, cc.SpriteAtlas, (_err, atlas) => {
@@ -63,21 +66,39 @@ export default class NewClass extends cc.Component {
         return Math.floor(Math.random() * n) + 1
     }
 
-    checkSeq(){
+    checkSeq() {
         let y = this.random(5)
-        if(this.seq[y]){
+        if (this.seq[y]) {
 
         }
 
     }
 
-    randomAvatar() {
+    randomAvatar(i) {
         let x = this.random(10)
-        console.log(this.avatarName[x]);
+        console.log(this.avatarName1[x]);
         let testPrefab = cc.instantiate(this.avatar)
-        this.com1.addChild(testPrefab)
-        this.picChange(testPrefab, this.path, this.avatarName[x])
-        
+
+        if (i == 1) {
+            this.com1.addChild(testPrefab)
+            this.picChange(testPrefab, this.path[1], this.avatarName2[x])
+        }else if (i == 2) {
+            this.com2.addChild(testPrefab)
+            this.picChange(testPrefab, this.path[0], this.avatarName1[x])
+        }else if (i == 3) {
+            this.com3.addChild(testPrefab)
+            this.picChange(testPrefab, this.path[0], this.avatarName1[x])
+        }else if (i == 4) {
+            this.com4.addChild(testPrefab)
+            this.picChange(testPrefab, this.path[0], this.avatarName1[x])
+        }else {
+            this.com5.addChild(testPrefab)
+            this.picChange(testPrefab, this.path[1], this.avatarName2[x])
+            testPrefab.is3DNode = true
+            testPrefab.eulerAngles = cc.v3(x, 180, 0)
+        }
+
+
 
 
         // for (let i = 0; i < 6; i++) {
@@ -89,8 +110,8 @@ export default class NewClass extends cc.Component {
         //     this.picChange(this.avatar, this.path, this.avatarName[x])
         //     let testPrefab = cc.instantiate(this.avatar)
         //     console.log(`com${i}`);
-            
-		// 	this[`com${i}`].addChild(testPrefab)
+
+        // 	this[`com${i}`].addChild(testPrefab)
         // }
     }
 
