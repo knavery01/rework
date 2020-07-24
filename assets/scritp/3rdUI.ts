3f5c59eba36ea2764fe8200a6f848ea6b4eee26e
anawat <s5953410018@sau.ac.th>
2020-07-24 18:16:09 +0700

    seqPlayer

--- assets/scritp/3rdUI.ts
+++ assets/scritp/3rdUI.ts
@@ -31,6 +31,20 @@ export default class NewClass extends cc.Component {
     @property(cc.Node)
     layout: cc.Node
 
+    @property(cc.Label)
+    seqCom1: cc.Label
+
+    @property(cc.Label)
+    seqCom2: cc.Label
+
+    @property(cc.Label)
+    seqCom3: cc.Label
+
+    @property(cc.Label)
+    seqCom4: cc.Label
+
+    @property(cc.Label)
+    seqCom5: cc.Label
 
 
     // LIFE-CYCLE CALLBACKS:
@@ -38,19 +52,26 @@ export default class NewClass extends cc.Component {
     // onLoad () {}
 
     start() {
-        for (let i = 0; i <= 4; i++) {
-            this.randomAvatar(i)
-            console.log(i);
-
-        }
+        this.checkSeq()
+        this.getAvatar()
+        
 
     }
     seq = [0, 1, 2, 3, 4, 5]
+    newSeq = []
 
     avatarName1 = ['captain_america_front', 'chuwit_front', 'kim_front', 'mark_front', 'monlo_front', 'pablo_front', 'prayut_front', 'tanee_front', 'trump_front', 'wonder_woman_front', 'xi_jinping_front']
     avatarName2 = ['captain_america_side', 'chuwit_side', 'kim_side', 'mark_side', 'monlo_side', 'pablo_side', 'prayut_side', 'tanee_side', 'trump_side', 'wonder_woman_side', 'xi_jinping_side']
 
-    path = ['Avatar/Avatar_Front','Avatar/Avatar_Side']
+    path = ['Avatar/Avatar_Front', 'Avatar/Avatar_Side']
+
+    getAvatar() {
+        for (let i = 0; i <= 5; i++) {
+            this.randomAvatar(i)
+            console.log(i);
+
+        }
+    }
 
     getImageRes(node, url, name) {
         cc.loader.loadRes(url, cc.SpriteAtlas, (_err, atlas) => {
@@ -68,10 +89,19 @@ export default class NewClass extends cc.Component {
 
     checkSeq() {
         let y = this.random(5)
-        if (this.seq[y]) {
+        console.log('seq player : '+y);
+        
+        this.seq[y]=y 
+            for(let z = y+1; z<=5;z++){
+                this.newSeq.push(z)
+            }
+            for(let z = 0; z<=y-1;z++){
+                this.newSeq.push(z)
+            }
 
-        }
-
+        
+        console.log(this.newSeq);
+        
     }
 
     randomAvatar(i) {
@@ -82,20 +112,27 @@ export default class NewClass extends cc.Component {
         if (i == 1) {
             this.com1.addChild(testPrefab)
             this.picChange(testPrefab, this.path[1], this.avatarName2[x])
-        }else if (i == 2) {
+            this.seqCom1.string = 'Seq '+this.newSeq[0]
+        } else if (i == 2) {
             this.com2.addChild(testPrefab)
             this.picChange(testPrefab, this.path[0], this.avatarName1[x])
-        }else if (i == 3) {
+            this.seqCom2.string = 'Seq '+this.newSeq[1]
+        } else if (i == 3) {
             this.com3.addChild(testPrefab)
             this.picChange(testPrefab, this.path[0], this.avatarName1[x])
-        }else if (i == 4) {
+            this.seqCom3.string = 'Seq '+this.newSeq[2]
+        } else if (i == 4) {
             this.com4.addChild(testPrefab)
             this.picChange(testPrefab, this.path[0], this.avatarName1[x])
-        }else {
+            this.seqCom4.string = 'Seq '+this.newSeq[3]
+        } else if (i == 5) {
             this.com5.addChild(testPrefab)
             this.picChange(testPrefab, this.path[1], this.avatarName2[x])
             testPrefab.is3DNode = true
             testPrefab.eulerAngles = cc.v3(x, 180, 0)
+            this.seqCom5.string = 'Seq '+this.newSeq[4]
+        }else{
+
         }
 
 
